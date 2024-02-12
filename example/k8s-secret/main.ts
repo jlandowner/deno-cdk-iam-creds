8da887b5c967aa07d989b6a2fc13106081dbaa5c
jlandowner <jlandowner8@gmail.com>
2024-02-13 01:36:14 +0900

    Update Example

--- example/k8s-secret/main.ts
+++ example/k8s-secret/main.ts
@@ -2,7 +2,7 @@ import { encodeBase64 } from "https://deno.land/std@0.215.0/encoding/base64.ts";
 import { stringify } from "https://deno.land/std@0.215.0/yaml/mod.ts";
 import { getIAMCredentialSecretByStack } from "https://raw.githubusercontent.com/jlandowner/deno-cdk-iam-creds/main/mod.ts";
 
-const secret = await getIAMCredentialSecretByStack("IAMUserStack");
+const secret = await getIAMCredentialSecretByStack(Deno.args[0] || "IAMUserStack");
 
 const b64AccessKey = encodeBase64(secret.AWS_ACCESS_KEY_ID);
 const b64SecretAccessKey = encodeBase64(secret.AWS_SECRET_ACCESS_KEY);
