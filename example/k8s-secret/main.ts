b32a8a8d2e487bc1374809475d88329f07431308
JINU <info.j.landwoner.k@gmail.com>
2024-03-14 00:05:25 +0900

    Update main.ts

--- example/k8s-secret/main.ts
+++ example/k8s-secret/main.ts
@@ -3,6 +3,7 @@ import { stringify } from "https://deno.land/std@0.215.0/yaml/mod.ts";
 import { getIAMCredentialSecretByStack } from "https://raw.githubusercontent.com/jlandowner/deno-cdk-iam-creds/main/mod.ts";
 
 const secret = await getIAMCredentialSecretByStack(Deno.args[0] || "IAMUserStack");
+const name = Deno.args[1] || "aws-credentials-secret";
 
 const b64AccessKey = encodeBase64(secret.AWS_ACCESS_KEY_ID);
 const b64SecretAccessKey = encodeBase64(secret.AWS_SECRET_ACCESS_KEY);
@@ -11,7 +12,7 @@ const sec = stringify({
   apiVersion: "v1",
   kind: "Secret",
   metadata: {
-    name: "aws-credentials-secret",
+    name: name,
   },
   data: {
     AWS_ACCESS_KEY_ID: b64AccessKey,
